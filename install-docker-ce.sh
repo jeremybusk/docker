711eebf313452854b69242828effd6de7b669ef1
Jeremy Busk <jeremy@pyrofex.net>
2019-04-30 12:20:22 -0600

    Update install-docker-ce.sh

--- install-docker-ce.sh
+++ install-docker-ce.sh
@@ -14,3 +14,6 @@ sudo add-apt-repository \
    
 sudo apt-get update
 sudo apt-get -y install docker-ce
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
